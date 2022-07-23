9fd2eadc6addc36d23a6aabbf4926a754fb6e77a
Ilka Lorena Air <IlkaLorenaAir@Ilkas-MacBook-Air.local>
2022-07-22 22:43:52 -0400

    Homepage Display

--- 3-React/nucampsite/src/components/Navigation.js
+++ 3-React/nucampsite/src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Col } from "reactstrap";
 const Navigation = () => {
     return (
         <Col xs={{ size: 4, offset: 1 }} sm='2' >
-            <h5>this will be site navigation!</h5>
+            <h5>This will be site navigation!</h5>
         </Col>
     )
 }
